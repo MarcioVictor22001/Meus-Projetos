998a9387f0eecb7c6eb30dfa499167357bfda8cf
marciovictor22001 <marciovictor22001@gmail.com>
2022-06-25 16:24:40 -0300

    .

--- Site de baixar musica/app.js
+++ Site de baixar musica/app.js
@@ -1,8 +1,10 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
+function myFunction(event) {
+    let menu = document.getElementById("menu");
+
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+      event.preventDefault();
+    }else{
+      menu.style.display = "block";
     }
   }
