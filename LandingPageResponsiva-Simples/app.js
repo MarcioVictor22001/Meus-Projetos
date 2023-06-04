070dedea45682209b5d87cce42ee8e9dc1fae56d
Marcio Victor de Oliveira Pinheiro <75379947+MarcioVictor22001@users.noreply.github.com>
2023-06-04 02:30:16 -0300

    Add files via upload

---
+++ LandingPageResponsiva-Simples/app.js
@@ -0,0 +1,6 @@
+const btnMenu = document.querySelector('.menu i');
+const navMenu = document.querySelector('.nav-menu');
+
+btnMenu.addEventListener('click', ()=>{
+      navMenu.classList.toggle('ativo')
+})
